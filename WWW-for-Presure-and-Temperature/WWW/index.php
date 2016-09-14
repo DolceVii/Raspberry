b7503c6111f95ec3aeab0c66d0b33ac854126e8d
DolceVii <dolcevii@hotmail.com>
2016-09-14 06:05:37 +0300

    Create index.php

---
+++ WWW-for-Presure-and-Temperature/WWW/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Charts</title>
+</head>
+<frameset cols="50%,50%">
+   <frame name="left" src="presure.php" />
+   <frame name="right" src="temperature.php" />
+   <noframes>
+   <body>
+      Your browser does not support frames.
+   </body>
+   </noframes>
+</frameset>
+</html>
