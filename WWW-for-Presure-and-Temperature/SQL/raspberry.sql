993c49d715d16097746eca5b856c57640b5c471c
DolceVii <dolcevii@hotmail.com>
2016-09-14 06:03:39 +0300

    Add files via upload

---
+++ WWW-for-Presure-and-Temperature/SQL/raspberry.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2+deb8u2
+-- http://www.phpmyadmin.net
+--
+-- Φιλοξενητής: localhost
+-- Χρόνος δημιουργίας: 10 Σεπ 2016 στις 05:19:09
+-- Έκδοση διακομιστή: 5.5.50-0+deb8u1
+-- Έκδοση PHP: 5.6.24-0+deb8u1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Βάση δεδομένων: `raspberry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `Temperature`
+--
+
+CREATE TABLE IF NOT EXISTS `Temperature` (
+`id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `temperature` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `Presure`
+--
+
+CREATE TABLE IF NOT EXISTS `Presure` (
+`id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `presure` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Ευρετήρια για άχρηστους πίνακες
+--
+
+--
+-- Ευρετήρια για πίνακα `Temperature`
+--
+ALTER TABLE `Temperature`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Ευρετήρια για πίνακα `Voltage`
+--
+ALTER TABLE `Presure`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT για άχρηστους πίνακες
+--
+
+--
+-- AUTO_INCREMENT για πίνακα `Temperature`
+--
+ALTER TABLE `Temperature`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT για πίνακα `Voltage`
+--
+ALTER TABLE `Presure`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
